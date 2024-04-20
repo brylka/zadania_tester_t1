115ffa1a1141234f7293138700b1f6a50450558c
Bartosz Bryniarski <brylka@jelenia.com>
2024-04-20 13:59:59 +0200

    add if in function

--- funkcje.py
+++ funkcje.py
@@ -1,7 +1,18 @@
 def witaj(imie):
     print(f"Witaj {imie}!")
 
-def idz_umyj_rece():
+def idz_umyj_rece(jak_brudne):
     print("Idę umyć ręce!")
+    if jak_brudne == 'czyste':
+        print('Po co przychodzisz do łazienki jak masz czyste ręce?')
+    elif jak_brudne == 'lekko':
+        print('Myję ręce mydłem zapachowym.')
+    elif jak_brudne == 'bardzo':
+        print('Myję ręce pastą bhp!')
+    else:
+        print("Sprecyzuj jak brudne masz ręce!")
+        idz_umyj_rece(input("Jak brudne masz ręce? (czyste/lekko/bardzo): "))
 
-witaj(input("Podaj swoje imię: "))
+#witaj(input("Podaj swoje imię: "))
+
+idz_umyj_rece(input("Jak brudne masz ręce? (czyste/lekko/bardzo): "))
