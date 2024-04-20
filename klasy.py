42f24e4f48d021807c1af89df210170691ae754e
Bartosz Bryniarski <brylka@jelenia.com>
2024-04-20 15:00:33 +0200

    add calss Test

--- klasy.py
+++ klasy.py
@@ -6,7 +6,14 @@ class Osoba:
         print(f"Nazywam sie {self.imie}!")
     def ile_masz_lat(self):
         print(f"Mam lat {self.wiek}.")
+    def zmien_imie(self,zzz):
+        self.imie = zzz
+    def dodaj(self,a,b):
+        print(f"Ja {self.imie} twierdzę, że: {a} + {b} = {a + b}")
 
+class Test:
+    def odejmij(self, c, d):
+        return c - d
 #Osoba().przedstaw_sie()
 
 bartosz = Osoba("Bartosz", 45)
@@ -16,3 +23,10 @@ bartosz.ile_masz_lat()
 pawel = Osoba("Paweł", 30)
 pawel.przedstaw_sie()
 pawel.ile_masz_lat()
+
+bartosz.zmien_imie("Aleksander")
+bartosz.przedstaw_sie()
+bartosz.dodaj(10,20)
+pawel.dodaj(10,20)
+
+print(Test().odejmij(30,20))
